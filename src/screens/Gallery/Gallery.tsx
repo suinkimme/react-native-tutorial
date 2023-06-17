569386a0976dbd1c4397881679568f59fda8eeae
Suin Kim <contact@suinkim.me>
2023-06-17 16:32:16 +0900

    retriever.release(); 예외 처리함

--- src/screens/Gallery/Gallery.tsx
+++ src/screens/Gallery/Gallery.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
+import { PermissionsAndroid, Platform } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 // types
@@ -11,6 +12,28 @@ export type GalleryProps = NativeStackScreenProps<
 >;
 
 const Gallery = ({}: GalleryProps) => {
+  const hasAndroidPermission = async () => {
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+
+    const hasPermission = await PermissionsAndroid.check(permission);
+    if (hasPermission) {
+      return true;
+    }
+
+    const status = await PermissionsAndroid.request(permission);
+    return status === 'granted';
+  };
+
+  const getPhotoWithPermission = async () => {
+    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getPhotoWithPermission();
+  }, []);
+
   return <Text>Gallery</Text>;
 };
 
