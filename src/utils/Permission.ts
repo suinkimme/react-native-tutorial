bf7e4a7d7a95ccfe2555011ef57c1678d1375765
Suin Kim <contact@suinkim.me>
2023-06-18 22:53:50 +0900

    간단 설명 추가

---
+++ src/utils/Permission.ts
@@ -0,0 +1,110 @@
+// Android
+// AndroidManifest.xml
+/*** 사용할 권한만 추가하면 됨 ***/
+/*
+<uses-permission android:name="android.permission.ACCEPT_HANDOVER" />
+<uses-permission android:name="android.permission.ACCESS_BACKGROUND_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_MEDIA_LOCATION" />
+<uses-permission android:name="android.permission.ACTIVITY_RECOGNITION" />
+<uses-permission android:name="android.permission.ANSWER_PHONE_CALLS" />
+<uses-permission android:name="android.permission.BLUETOOTH_ADVERTISE" />
+<uses-permission android:name="android.permission.BLUETOOTH_CONNECT" />
+<uses-permission android:name="android.permission.BLUETOOTH_SCAN" />
+<uses-permission android:name="android.permission.BODY_SENSORS" />
+<uses-permission android:name="android.permission.CALL_PHONE" />
+<uses-permission android:name="android.permission.CAMERA" />
+<uses-permission android:name="android.permission.GET_ACCOUNTS" />
+<uses-permission android:name="android.permission.PROCESS_OUTGOING_CALLS" />
+<uses-permission android:name="android.permission.READ_CALENDAR" />
+<uses-permission android:name="android.permission.READ_CALL_LOG" />
+<uses-permission android:name="android.permission.READ_CONTACTS" />
+<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+<uses-permission android:name="android.permission.READ_PHONE_NUMBERS" />
+<uses-permission android:name="android.permission.READ_PHONE_STATE" />
+<uses-permission android:name="android.permission.READ_SMS" />
+<uses-permission android:name="android.permission.RECEIVE_MMS" />
+<uses-permission android:name="android.permission.RECEIVE_SMS" />
+<uses-permission android:name="android.permission.RECEIVE_WAP_PUSH" />
+<uses-permission android:name="android.permission.RECORD_AUDIO" />
+<uses-permission android:name="android.permission.SEND_SMS" />
+<uses-permission android:name="android.permission.USE_SIP" />
+<uses-permission android:name="android.permission.WRITE_CALENDAR" />
+<uses-permission android:name="android.permission.WRITE_CALL_LOG" />
+<uses-permission android:name="android.permission.WRITE_CONTACTS" />
+<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+<uses-permission android:name="com.android.voicemail.permission.ADD_VOICEMAIL" />
+*/
+
+// iOS
+// Podfile
+/*
+permissions_path = '../node_modules/react-native-permissions/ios' # react-native-permissions 패키지 쓰면 필수
+
+pod 'Permission-AppTrackingTransparency', :path => "#{permissions_path}/AppTrackingTransparency"
+pod 'Permission-BluetoothPeripheral', :path => "#{permissions_path}/BluetoothPeripheral"
+pod 'Permission-Calendars', :path => "#{permissions_path}/Calendars"
+pod 'Permission-Camera', :path => "#{permissions_path}/Camera"
+pod 'Permission-Contacts', :path => "#{permissions_path}/Contacts"
+pod 'Permission-FaceID', :path => "#{permissions_path}/FaceID"
+pod 'Permission-LocationAccuracy', :path => "#{permissions_path}/LocationAccuracy"
+pod 'Permission-LocationAlways', :path => "#{permissions_path}/LocationAlways"
+pod 'Permission-LocationWhenInUse', :path => "#{permissions_path}/LocationWhenInUse"
+pod 'Permission-MediaLibrary', :path => "#{permissions_path}/MediaLibrary"
+pod 'Permission-Microphone', :path => "#{permissions_path}/Microphone"
+pod 'Permission-Motion', :path => "#{permissions_path}/Motion"
+pod 'Permission-Notifications', :path => "#{permissions_path}/Notifications"
+pod 'Permission-PhotoLibrary', :path => "#{permissions_path}/PhotoLibrary"
+pod 'Permission-PhotoLibraryAddOnly', :path => "#{permissions_path}/PhotoLibraryAddOnly"
+pod 'Permission-Reminders', :path => "#{permissions_path}/Reminders"
+pod 'Permission-Siri', :path => "#{permissions_path}/Siri"
+pod 'Permission-SpeechRecognition', :path => "#{permissions_path}/SpeechRecognition"
+pod 'Permission-StoreKit', :path => "#{permissions_path}/StoreKit"
+*/
+
+// Info.plist
+/*** 사용할 권한만 추가하면 됨 ***/
+/*
+<key>NSAppleMusicUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSBluetoothAlwaysUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSBluetoothPeripheralUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSCalendarsUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSCameraUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSContactsUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSFaceIDUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSLocationAlwaysAndWhenInUseUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSLocationAlwaysUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSLocationTemporaryUsageDescriptionDictionary</key>
+<dict>
+  <key>YOUR-PURPOSE-KEY</key>
+  <string>YOUR TEXT</string>
+</dict>
+<key>NSLocationWhenInUseUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSMicrophoneUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSMotionUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSPhotoLibraryUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSPhotoLibraryAddUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSRemindersUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSSpeechRecognitionUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSSiriUsageDescription</key>
+<string>YOUR TEXT</string>
+<key>NSUserTrackingUsageDescription</key>
+<string>YOUR TEXT</string>
+*/
